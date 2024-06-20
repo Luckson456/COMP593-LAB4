f25d4184a576050bc4fd6cf58737fb250b026029
Luckson456 <lpaija@flemingcollege.ca>
2024-06-20 18:24:39 -0400

    completed generate port traffic report

--- log_investigation.py
+++ log_investigation.py
@@ -10,7 +10,8 @@ Parameters:
 """
 import log_analysis_lib
 import csv
-
+import re
+from datetime import datetime
 
 # Get the log file path from the command line
 # Because this is outside of any function, log_path is a global variable
@@ -31,7 +32,7 @@ def main():
     # Generate log of records from source IP 220.195.35.40
     generate_source_ip_log('220.195.35.40')
 
-def tally_port_traffic():
+def tally_port_traffic(log_path,regex_pattern):
     """Produces a dictionary of destination port numbers (key) that appear in a 
     specified log file and a count of how many times they appear (value)
 
@@ -39,25 +40,59 @@ def tally_port_traffic():
         dict: Dictionary of destination port number counts
     """
     # TODO: Complete function body per step 7
-    return {}
+    port_traffic={}
+    extracted_data=[]
+    with open (log_path,'r' )as log_file:
+        for line in log_file:
+            match=re.search(regex_pattern,line)
+            if match:
+                extracted_values=match.groups()
+                extracted_data.append(extracted_values)
+                port=int(extracted_values[-1])
+                port_traffic[port]=port_traffic.get(port,0)+1
+                            
+    return {port_traffic,extracted_data}
 
-def generate_port_traffic_report(port_number):
+def generate_port_traffic_report(log_path ,port_number):
     """Produces a CSV report of all network traffic in a log file for a specified 
     destination port number.
 
     Args:
         port_number (str or int): Destination port number
     """
+    
     # TODO: Complete function body per step 8
+    regex_pattern =r'SRC=(.*?) DST=(.*?)LEN=(.*?)'
+    extracted_data =[]
+    
+    with open(log_path,'r') as log_file:
+        for line  in log_file:
+            match= re.search(regex_pattern,line)
+            if match:
+                src,dst,length =match.groups()
+                dst_port=int(dst.split('.')[-1])
+                if dst_port==int(port_number):
+                    extracted_data.append((src,dst,length))
+                    
+    if extracted_data:
+        report_path=f'port_{port_number}_traffic_report.csv'
+        with open(report_path,'w',newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['Source IP','Destination IP',length])
+            writer.writerows(extracted_data)
+        
     # Get data from records that contain the specified destination port
     # Generate the CSV report
     return
 
-def generate_invalid_user_report():
+def generate_invalid_user_report(log_path,regex_pattern):
     """Produces a CSV report of all network traffic in a log file that show
     an attempt to login as an invalid user.
     """
     # TODO: Complete function body per step 10
+    extracted_data=[]
+    
+    with open(log_path,'r')as log_file:
     # Get data from records that show attempted invalid user login
     # Generate the CSV report
     return
